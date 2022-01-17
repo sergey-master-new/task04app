572ff205e561a1119e80e6dfbe5a20f921001714
sergey-master-new <prodvizh.com@gmail.com>
2022-01-17 14:17:28 +0300

    Initial commit

---
+++ src/main/java/com/example/task04app/Task04appApplication.java
@@ -0,0 +1,13 @@
+package com.example.task04app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task04appApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task04appApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task04app/Task04appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task04app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task04appApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
